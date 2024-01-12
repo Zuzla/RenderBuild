16ce6c0d63ff3c557cc67a2ebc6912272f283055
Zuzla <ilyagaevsiy@icloud.com>
2024-01-12 11:35:29 +0000

    Add renders and raytracers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# RenderBuild
+# Simple Raytracing and Rendering Learning Project
+This project is designed to provide an immersive and hands-on experience in the development of a basic raytracer and renderer.    
+The main goal of this project is to understand the basic concepts of ray tracing and rendering.
+
+# Links
+- https://github.com/scratchapixel/code/tree/main
---
+++ TinySoftwareRendering/src/gl.h
@@ -0,0 +1,21 @@
+#pragma once 
+#include "tgaimage.h"
+#include "geometry.h"
+
+extern Matrix ModelView;
+extern Matrix Projection;
+extern Matrix Viewport;
+
+void viewport(int x, int y, int w, int h);
+void projection(float coeff=0.f); // coeff = -1/c
+void lookat(Vec3f eye, Vec3f center, Vec3f up);
+
+class IShader {
+public:
+    virtual ~IShader();
+    virtual Vec4f vertex(int iface, int nthvert) = 0;
+    virtual bool fragment(Vec3f gl_FragCoord, Vec3f bar, TGAColor &color) = 0;
+};
+
+//void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
+void triangle(mat<4,3,float> &pts, IShader &shader, TGAImage &image, float *zbuffer);
